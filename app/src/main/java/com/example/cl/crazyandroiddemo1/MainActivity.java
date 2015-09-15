5fbd4517a401a990d010723678c55c33f400a207
chinadarren <chinadarren@outlook.com>
2015-09-15 15:59:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cl/crazyandroiddemo1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cl.crazyandroiddemo1;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+     public void clickHandler(View source){
+         TextView tv = (TextView) findViewById(R.id.show);
+         //修改文本框的内容
+         tv.setText("Hello Android-"+new java.util.Date());
+}
+}
